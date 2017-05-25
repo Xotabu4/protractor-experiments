524cf8632126f8fb4750f856b5d0979a14805169
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-05-25 16:52:22 +0300

    Reporter itself
    Reporter itself

--- experiments/report_portal/reportPortalReporter.ts
+++ experiments/report_portal/reportPortalReporter.ts
@@ -2,10 +2,13 @@ var Service = require("reportportal-client");
 
 export class ReportPortal {
     private RPclient
+    private launchID
+    private currentSuite
+    private currentTest
 
     constructor() {
         const rpConfig = {
-            endpoint: "http://localhost:8080",
+            endpoint: "http://localhost:8080/api/v1",
             project: "default_personal",
             token: "4044a955-30da-499e-a42b-945009af17bb",
             launch: "TEST LAUNCH"
@@ -15,21 +18,43 @@ export class ReportPortal {
     }
 
     async jasmineStarted(suiteInfo) {
-        var startLaunchResponse = await this.RPclient.startLaunch({
-            name: "JS tests experiments",
-        });
+        let resp = await this.RPclient.startLaunch({
+            start_time: new Date().valueOf(),
+            name: "JS TEST NAME",
+        })
+        console.log(resp)
+        this.launchID = resp.id
     }
 
-    suiteStarted(suite) {
+    async suiteStarted(suite: any) {
+        console.log(this.launchID)
+        this.currentSuite = suite
+        let requestData = {
+            name: "OLOLONAME SUITE",
+            launch_id: this.launchID,
+            start_time: new Date().valueOf(),
+            type: "SUITE",
+            description: this.currentSuite.fullName,
+        }
 
+        let resp = await this.RPclient.startTestItem(requestData)
+        console.log(resp)
     }
 
-    specStarted(spec) {
-
+    async specStarted(spec) {
+        let requestData = {
+            name: "OLOLONAME TEST",
+            launch_id: this.launchID,
+            start_time: new Date().valueOf(),
+            type: "TEST",
+            description: 'MY TEST NAME!',
+        }
+        let resp = await this.RPclient.startTestItem(requestData)
+        console.log(resp)
     }
 
     specDone(result) {
-
+        //this.RPclient.finishTestItem()
     }
 
     suiteDone(suite) {
@@ -40,3 +65,12 @@ export class ReportPortal {
         console.log('Finished Jasmine');
     }
 }
+
+let reporter = new ReportPortal()
+async function doRun() {
+    await reporter.jasmineStarted({})
+    await reporter.suiteStarted({})
+    await reporter.specStarted({})
+}
+//doRun()
+
