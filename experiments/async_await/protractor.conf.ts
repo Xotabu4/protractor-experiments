b61bf1420f7b02ede4c8427a93b8242baa711f30
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-07-09 23:39:43 +0300

    run without compile - trying some tricks
    run without compile - trying some tricks

--- experiments/async_await/protractor.conf.ts
+++ experiments/async_await/protractor.conf.ts
@@ -4,7 +4,7 @@ import { Config, ExpectedConditions, $ } from 'protractor'
 let conf: Config = {
   directConnect: true,
   baseUrl: 'http://www.protractortest.org/testapp/ng1/#/form',
-  specs: ['./experiment.js'],
+  specs: ['./experiment.ts'],
   SELENIUM_PROMISE_MANAGER: false
 };
 
