77c06acee83c5e36e3a188f49796c4031a553beb
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-01-19 16:57:47 +0200

    Safari parallel
    Safari parallel

---
+++ experiments/safari_parallel/protractor.conf.ts
@@ -0,0 +1,28 @@
+import { Config, ExpectedConditions, $ } from 'protractor'
+
+declare const browser: any
+let conf: Config = {
+    //directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    baseUrl: 'http://www.protractortest.org/testapp/ng1/#/form',
+    specs: ['./tests/*.js'],
+    SELENIUM_PROMISE_MANAGER: false,
+    capabilities:
+        {
+            browserName: 'safari',
+            shardTestFiles: true,
+            maxInstances: 3
+        },
+
+    onPrepare: async function () {
+        await browser.waitForAngularEnabled(false)
+    },
+
+    onComplete: async function () {
+        await browser.close()
+    }
+
+    
+};
+
+exports.config = conf;
---
+++ experiments/safari_parallel/tests/experiment.1.ts
@@ -0,0 +1,9 @@
+import {browser, $, by, element} from 'protractor'
+describe('async/await', ()=> {
+
+    it('testing rejected promises', async function () {
+        await browser.get('')
+        await browser.sleep(5000)
+        await $('body').getText()
+    })
+})
